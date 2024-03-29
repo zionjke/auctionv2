c14e4a8cb3bf550958a4daf3851142791b7a41b7
vart <vart@jooble.com>
2024-03-29 17:14:13 +0200

    1.4 add react, style-loader

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import {render} from "react-dom";
+
+render(
+    <h1>Hello, World!</h1>,
+    document.getElementById("root")
+);
--- webpack.config.ts
+++ webpack.config.ts
@@ -9,7 +9,7 @@ export default (env: BuildEnv) => {
     const options: BuildOptions = {
         mode: env.mode ||  'development',
         paths: {
-            entry: path.resolve(__dirname, 'src', 'index.ts'),
+            entry: path.resolve(__dirname, 'src', 'index.tsx'),
             output: path.resolve(__dirname, 'dist'),
             html: path.resolve(__dirname, 'public', 'index.html')
         },
