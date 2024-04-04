014d23fed2f349025779657aad4e72d25db10b60
vart <vart@jooble.com>
2024-04-04 18:03:39 +0300

    1.7 add theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React, {lazy, Suspense} from 'react';
 import {Routes, Route, Link} from "react-router-dom";
-import './index.scss';
+import './styles/index.scss';
+import {useTheme} from "./theme/useTheme";
 
 const About = lazy(() => import("./pages/AboutPage/AboutPage"));
 const Main = lazy(() => import("./pages/Main/MainPage"));
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
             <Link to="/">Main</Link>
             <Link to="/about">About</Link>
+            <button onClick={toggleTheme}>Toggle theme</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path="/about" element={<About/>}/>
