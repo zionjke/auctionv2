1abc5088446fdbca9a7b8e9aa9ef727a3cb593c6
vart <vart@jooble.com>
2024-04-10 14:54:58 +0300

    2.1 add material-ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
-import React, {Suspense} from 'react';
-import {Routes, Route, Link} from "react-router-dom";
+import React from 'react';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames";
-import {MainPage} from "pages/Main";
 import {AppRouter} from "app/providers/Router";
 
 
 const App = () => {
     return (
         <div className={classNames('app', {}, [])}>
-            <Link to="/">Main</Link>
             <AppRouter/>
         </div>
     );
